a123ce865458bcca869c733b47e2e3787b2f29d9
AlexSousa <alex.sousa20@hotmail.com>
2020-07-24 09:31:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# CRUD in Javascript
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+Um simples CRUD usando Javascript.
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#deployment) for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+
+
+```
+git 
+live-server
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running.
+
+Say what the step will be
+
+```
+clone https://github.com/alex-dev2015/crud_javascript.git
+```
+
+
+
+End with an example of getting some data out of the system or using it for a little demo.
+
+## Usage <a name = "usage"></a>
+
+
---
+++ js/script.js
@@ -0,0 +1,118 @@
+window.addEventListener('load', start);
+
+var globalNames = ['Alex Sousa', 'João Dias', 'Alberto Almeida', 'Bartolomeu Júnior'];
+var inputName = null;
+var isEditing = false;
+var isEditing = null;
+
+function start(){
+    inputName = document.querySelector('#name_user');
+    preventFormSubmit();
+    activateInput();
+    render();
+    
+}
+
+function preventFormSubmit(){
+    function handleFormSubmit(event){
+        event.preventDefault();
+        
+    }
+    
+    var form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput(){
+    function insertName(newName){
+        globalNames.push(newName);
+        
+    }
+
+    function updateName(newName){
+        globalNames[currentIndex] = newName;
+        
+    }
+
+    function handleTyping(event){
+      if(event.key === 'Enter' && event.target.value.trim()!=='')
+      {
+          if(isEditing){
+            updateName(event.target.value);
+          }else{
+            insertName(event.target.value);  
+          }
+        
+          render();
+          isEditing = false;
+          clearInput();
+      }
+    }
+
+    inputName.addEventListener('keyup' , handleTyping);
+    inputName.focus();
+}
+
+function render(){
+   function createDeleteButton(index){
+      function deleteName(){
+          console.log(index);
+         globalNames.splice(index, 1);
+         render();
+      }  
+
+    var button = document.createElement('button');
+    button.classList.add('deleteButton');
+    button.textContent = 'X';
+
+    button.addEventListener('click', deleteName);
+
+    return button;
+   } 
+
+   function createSpan(name, index){
+      function editItem(){
+          inputName.value = name;
+          inputName.focus();
+          isEditing = true;
+          currentIndex = index;
+      }  
+
+    var span = document.createElement('span');
+    span.classList.add('clickable');
+    span.textContent = currentName; 
+    span.addEventListener('click', editItem);
+
+    return span;
+   }
+
+
+   var divNames = document.querySelector('#names'); 
+   divNames.innerHTML = ''; 
+  
+   var ul   = document.createElement('ul');
+   
+   for(var i = 0; i < globalNames.length; i++){
+      var currentName = globalNames[i]; 
+      var li = document.createElement('li');
+      var button = createDeleteButton(i);
+      var span = createSpan(currentName, i);
+      
+       
+      
+      li.appendChild(button);
+      li.appendChild(span);
+      
+      ul.appendChild(li);
+   }
+   
+ 
+   divNames.appendChild(ul);
+   clearInput();
+}
+
+function clearInput(){
+   inputName.value = '';
+   inputName.focus(); 
+}
+
